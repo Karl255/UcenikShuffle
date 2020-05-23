bb3f6ff9f3734c625457f4398815ae1c3516119b
ReflexLevel0 <dominikdejanovic2@gmail.com>
2020-05-24 01:53:30 +0200

    Fixed unit tests

--- UnitTesting/CommonTests/GroupTests.cs
+++ UnitTesting/CommonTests/GroupTests.cs
@@ -13,14 +13,15 @@ namespace UcenikShuffle.UnitTests.CommonTests
 			var shuffler = InsertTestData();
 			var students = shuffler.Students;
 
+			//IMPORTANT: this function might stop working if test data is changed
 			//Searching for groups of students in history
-			List<HashSet<Student>> groups = new List<HashSet<Student>>() {
+			var groups = new List<HashSet<Student>>() {
 				new HashSet<Student>() { students[0], students[1] },
 				new HashSet<Student>() { students[0], students[2] },
 				new HashSet<Student>() { students[1], students[2] }
 			};
-			var results = new int[] { 2, 1, 0 };
-			for (int i = 0; i < groups.Count; i++)
+			var results = new[] { 2, 1, 0 };
+			for (var i = 0; i < groups.Count; i++)
 			{
 				var result = Group.SearchGroupHistory(groups[i]);
 				Assert.Equal(results[i], result.Count());
@@ -32,9 +33,6 @@ namespace UcenikShuffle.UnitTests.CommonTests
 		{
 			var shuffler = InsertTestData();
 			var students = shuffler.Students;
-			students.Add(new Student());
-			students.Add(new Student());
-			students.Add(new Student());
 			var r1 = new HashSet<Student> { students[0], students[1] };
 			var r2 = new HashSet<Student> { students[0], students[2] };
 			Assert.True(Group.CompareGroupHistoryRecords(r1, r1));
@@ -43,9 +41,9 @@ namespace UcenikShuffle.UnitTests.CommonTests
 			Assert.False(Group.CompareGroupHistoryRecords(null, null));
 		}
 
-		static Shuffler InsertTestData()
+		private static Shuffler InsertTestData()
 		{
-			var shuffler = new Shuffler("1", "3");
+			var shuffler = new Shuffler(1, new[]{3});
 
 			var students = shuffler.Students;
 			Group.History.Add(new HashSet<Student>() { students[0], students[1] });
--- UnitTesting/CommonTests/StudentTests.cs
+++ UnitTesting/CommonTests/StudentTests.cs
@@ -4,22 +4,20 @@ using Xunit;
 
 namespace UcenikShuffle.UnitTests.CommonTests
 {
-	public class StudentTests
-	{
-		[Fact]
-		public void GetIndexOfId_ShouldWork()
-		{
-			var shuffler = new Shuffler("0", "2");
-
-			shuffler.Students.Add(new Student());
-			shuffler.Students.Add(new Student());
-			var students = (from s in shuffler.Students select s).ToList();
-			var expected = 0;
-			int actual = Student.GetIndexOfId(students, 1);
-			Assert.Equal(expected, actual);
-			expected = 1;
-			actual = Student.GetIndexOfId(students, 2);
-			Assert.Equal(expected, actual);
-		}
-	}
+    public class StudentTests
+    {
+        [Fact]
+        public void GetIndexOfId_ShouldWork()
+        {
+            var shuffler = new Shuffler(0, new[]{2});
+            
+            //Checking if Id's for created students are correct 
+            var expected = 0;
+            var actual = Student.GetIndexOfId(shuffler.Students, 1);
+            Assert.Equal(expected, actual);
+            expected = 1;
+            actual = Student.GetIndexOfId(shuffler.Students, 2);
+            Assert.Equal(expected, actual);
+        }
+    }
 }
