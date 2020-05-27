93a8842b35305634e7311499ab2fbcb58a3b5ffc
ReflexLevel0 <dominikdejanovic2@gmail.com>
2020-05-27 15:44:37 -0700

    More efficient shuffler

--- Common/HelperMethods.cs
+++ Common/HelperMethods.cs
@@ -1,63 +1,51 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace UcenikShuffle.Common
 {
-	public static class HelperMethods
-	{
-		/// <summary>
-		/// This method returns all possible number combinations for a certain group size
-		/// </summary>
-		/// <param name="groupSize">Size of the group (for example, if there are 5 numbers and group size is 3, only 3 of those numbers would be able to fit in a certain group combination)</param>
-		/// <param name="numberCount">Number of numbers for which all group combinations will be made (so if this parameter is 2 then every group combination can contain a maximum of one number 1 and one number 2 - no duplicates, no other numbers)</param>
-		/// <returns></returns>
-		public static IEnumerable<List<int>> GetAllNumberCombinations(int groupSize, int numberCount)
-		{
-			var group = new List<int>();
-
-			//Setting up the initial group
-			for (int i = 0; i < groupSize; i++)
-			{
-				group.Add(i);
-			}
-			yield return new List<int>(group);
-
-			//Going trough all combinations
-			while (true)
-			{
-				//Checking how many numbers need to be shifted
-				int counter = 1;
-				while (true)
-				{
-					//Shifting the current number
-					group[groupSize - counter]++;
-
-					//If current number is at it's maximum position, the previous number in the group will also be shifted
-					if (group[groupSize - counter] >= numberCount + 1 - counter)
-					{
-						counter++;
-
-						//If all combinations were tried out
-						if (counter > groupSize)
-						{
-							yield break;
-						}
-					}
-
-					//If no more numbers need to be shifted before the current one
-					else
-					{
-						break;
-					}
-				}
-
-				//Shifting numbers that need to be shifted (the first number that needed to be shifted was already shifter in the while loop, that's why there is +1 in for loop)
-				int beginningNumberIndex = groupSize - counter;
-				for (int i = beginningNumberIndex + 1; i < groupSize; i++)
-				{
-					group[i] = group[beginningNumberIndex] + i - beginningNumberIndex;
-				}
-				yield return new List<int>(group);
-			}
-		}
-	}
+    public static class HelperMethods
+    {
+        /// <summary>
+        /// This method returns all possible number combinations for a certain group size
+        /// </summary>
+        /// <param name="groupSize">Size of the group (for example, if there are 5 numbers and group size is 3, only 3 of those numbers would be able to fit in a certain group combination)</param>
+        /// <param name="numberCount">Number of numbers for which all group combinations will be made (so if this parameter is 2 then every group combination can contain a maximum of one number 1 and one number 2 - no duplicates, no other numbers)</param>
+        /// <returns></returns>
+        public static IEnumerable<List<int>> GetAllNumberCombinations(int groupSize, int numberCount)
+        {
+            int startNumber = 0;
+            var combination = new List<int>();
+			
+            //Initial combination
+            for (int i = 0; i < groupSize - 1; i++)
+            {
+                combination.Add(i);
+            }
+            while (true)
+            {
+                //If all combinations were returned
+                if (startNumber + groupSize > numberCount)
+                {
+                    break;
+                }
+				
+                //Changing the last number until it hits max number
+                for (int i = startNumber + groupSize - 1; i < numberCount; i++)
+                {
+                    combination.Add(i);
+                    //Note: new list must be created otherwise collection modified exception might thrown
+                    yield return new List<int>(combination);
+                    combination.Remove(i);
+                }
+				
+                //Popping the first number and adding another number to the base combination instead of the popped number
+                if (combination.Any())
+                {
+                    combination.RemoveAt(0);
+                    combination.Add(startNumber + groupSize - 1);
+                }
+                startNumber++;
+            }
+        }
+    }
 }
