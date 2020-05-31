d0b87f4c9a449d907b000a9eed767fd3683fdc5d
ReflexLevel0 <dominikdejanovic2@gmail.com>
2020-06-01 01:53:41 +0200

    Temp commit

--- Common/Shuffler.cs
+++ Common/Shuffler.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading;
+using UcenikShuffle.Common.Exceptions;
 
 namespace UcenikShuffle.Common
 {
@@ -38,20 +40,94 @@ namespace UcenikShuffle.Common
 			_progress = progress;
 
 			//Resetting group history after each shuffle
-			Group.ResetHistory();
+			Group.History = new List<HashSet<Student>>();
 
 			//Going trough each LV
 			_progress?.Report(0);
 			for (int lv = 0; lv < LvCount; lv++)
 			{
+				var historyCopy = Group.History.ToList();
+				var historyClearCheck = false;
 				var studentPool = new List<Student>(Students);
-				foreach (var group in Groups)
+				for(int i = 0; i < Groups.Count; i++)
 				{
-					group.AddStudents(studentPool, _cancellationSource,out studentPool, out var addedStudents);
+					Groups[i].AddStudents(studentPool, _cancellationSource,out studentPool, out var addedStudents, out bool historyClearSuggested);
 					ShuffleResult.Add(addedStudents);
+					if (historyClearSuggested)
+					{
+						historyClearCheck = true;
+					}
 				}
+				
+				//Checking if history needs to cleared
+				if (historyClearCheck)
+				{
+					var sizes = 
+						(from g in Groups 
+						select g.Size).OrderBy(s => s).Distinct();
+					
+					//Going trough each group size
+					foreach (var size in sizes)
+					{
+						bool clearHistory = true;
+						var filteredHistory = Group.History.Where(h => h.Count == size).ToList();
+						
+						//Checking if history for this group size needs to be cleared
+						foreach (var numberCombination in HelperMethods.GetAllNumberCombinations(size, Students.Count))
+						{
+							var studentCombination = new List<Student>(
+								from index in numberCombination
+								select Students[index]);
+							if (filteredHistory.Where(h => !h.Except(studentCombination).Any()).Any() == false)
+							{
+								clearHistory = false;
+								break;
+							}
+						}
+						
+						//If history for this group size needs to be cleared
+						if (clearHistory)
+						{
+							// var extraHistory = Group.History.Except(historyCopy);
+							// historyCopy.RemoveAll(h => h.Count == size);
+							// Group.History = new List<HashSet<Student>>(historyCopy);
+							// Group.History.AddRange(extraHistory);
+						}
+					}
+				}
+				
 				_progress?.Report((float)(lv + 1) / LvCount);
 			}
+
+			var tempResult = 
+				(from resultRow in ShuffleResult select 
+					(from student in resultRow 
+					select student.Id).ToList()).ToList();
+			//int err = 0;
+			for (int i = 0; i < Students.Count; i++)
+			{
+				Debug.WriteLine($"Student {i}");
+				for (int j = 0; j < Students.Count; j++)
+				{
+					if (i == j)
+					{
+						continue;
+					}
+					var count = 
+						(from r in tempResult
+						where r.Contains(i + 1) && r.Contains(j + 1)
+						select r).Count();
+					Debug.WriteLine(count);
+					// if (count < 140 || count > 160)
+					// {
+					// 	Debug.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+					// 	err += (count < 140) ? 140-count : count-160;
+					// }
+				}
+				Debug.WriteLine("");
+			}
+			//Debug.WriteLine(err);
+			
 			return ShuffleResult;
 		}
 	}
